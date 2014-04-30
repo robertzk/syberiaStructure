93c6cc5b1ffdfafdeebc783309e8aae7a73574c5
RobertZK <technoguyrob@gmail.com>
2014-04-30 17:12:26 -0500

    initial files for syberiaStructure

---
+++ R/config.r
@@ -0,0 +1,10 @@
+.github_packages <- list(
+  list('productivus', 'robertzk'),
+  list('Ramd', 'robertzk'),
+  list('frost', 'robertzk'),
+  list('stagerunner', 'robertzk'),
+  list('mungebitsTransformations', 'robertzk'),
+  list('mungebits', 'robertzk'),
+  list('tundra', 'robertzk')
+)
+
