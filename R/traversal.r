91efc22e1602d314bbf29500eb83f5afde06898f
RobertZK <technoguyrob@gmail.com>
2014-04-30 17:33:23 -0500

    need to refactor this a bit to make room for data source tests

--- R/traversal.r
+++ R/traversal.r
@@ -2,13 +2,17 @@
 #
 # By convention, the structure from a syberia root project will look like this:
 # - data    # Data preparation for data sources coming from an external API
-#   - data_source1
-#     - data_source1.r
-#     - helpers.r
-#   - data_source2.r 
+#   - sources
+#     - data_source1
+#       - data_source1.r
+#       - helpers.r
+#     - data_source2.r 
+#   - test  # Unit tests for data preparation for data sources
 # - models  # Files that parametrize the modeling process for each model version
 #   - dev   # The development environment to be used as a sandbox
 #   - prod  # Models will be copied over from dev to prod for deployment
 #   - test  # Unit tests for the models - should be environment-agnostic (dev or prod)
 # - syberia.config  # Configuration file for syberia project
+
+
 
