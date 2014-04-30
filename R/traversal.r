c6c1082e77e2b2e1e65dd2cc69c111bad791162d
RobertZK <technoguyrob@gmail.com>
2014-04-30 17:41:38 -0500

    try_memoize helper funtion

--- R/traversal.r
+++ R/traversal.r
@@ -17,3 +17,36 @@
 # - bin      # Production scripts
 # - syberia.config  # Configuration file for syberia project
 
+#' Determine the root path of the current Syberia project.
+#'
+#' The root of the Syberia project is considered to be the directory containing
+#' the \code{syberia.config} file.
+#'
+#' @param filename. If specified, it will attempt to find the Syberia root
+#'   relative to the file name by traversing up its parent directories.
+#'   If not given, Syberia will try to intelligently discern the current
+#'   Syberia project by first looking at the cache for the previously used
+#'   Syberia project and then by looking at the current directory. If no
+#'   project is found, this function will return NULL.
+#' @return see the \code{filename} parameter
+syberia_root <- function(filename = NULL) {
+  if (missing(filename))
+}
+
+#' Get syberia config relative to another source file.
+get_registry_dir <- function(source_file) {
+  prev_dir <- ""
+  dir <- dirname(source_file)
+  while(prev_dir != dir) {
+    if (file.exists(file.path(dir, "syberia.config"))) {
+      if (!file.exists(.syberia_dir <- file.path(dir, ".syberia")))
+        dir.create(.syberia_dir) # create .syberia directory
+      return(.syberia_dir)
+    }
+    prev_dir <- dir
+    dir <- dirname(prev_dir)
+  }
+  stop("No syberia.config file found -- please create it in the root ",
+       "of your syberia project.")
+}
+
--- R/utils.r
+++ R/utils.r
@@ -1,1 +1,19 @@
 `%||%` <- function(x, y) if (is.null(x)) y else x
+
+#' Attempt to memoize a function using the memoise package.
+#' 
+#' This function will load the \code{memoise} package if it is
+#' available, or do nothing otherwise.
+#'
+#' (Blame Hadley for the spelling of memoise.)
+#'
+#' @param fn function. The function to memoize.
+#' @return nothing, but \code{try_memoize} will use non-standard
+#'   evaluation to memoize in the calling environment.
+try_memoize <- function(fn) {
+  if ('memoise' %in% installed.packages()) {
+    require(memoise)
+    eval.parent(substitute(memoise(fn)))
+  }
+}
+
