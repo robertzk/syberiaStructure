4ffcb22ab45ab8b8d7a0aca24f3ac4ab43ccb9c9
RobertZK <technoguyrob@gmail.com>
2014-05-18 23:03:47 -0500

    syberia_resource function

--- R/resource.r
+++ R/resource.r
@@ -1,12 +1,12 @@
-#' Execute a file relative to a syberia directory.
+#' Fetch info about a file relative to a syberia directory.
 #'
 #' Some additional information will be returned. Namely, a list with
-#' two keys, `current` and `cached`. The former will contain information
-#' relating to the current execution of this file, while the latter
-#' contains information relating to the previous execution of this file.
-#' The ultimate use of this information depends on the caller, but potential
-#' applications include determining whether the contents of the file changed
-#' or whether the file has been modified on the file system.
+#' four keys, `current`, `cached`, `value`, and `modified`.
+#' The former two will contain information relating to the current
+#' and previous execution of this file, while `value` contains
+#' a function that will execute the file and return its output,
+#' and `modified` is a logical flag indicating whether or not the
+#' file has been modified since it was last executed by Syberia.
 #' 
 #' Internally, the cached information about the file is stored in the
 #' syberia project's registry under the key \code{resources/resource_cache}.
@@ -17,54 +17,55 @@
 #'   look for \code{filename}. The default is \code{syberia_root()}.
 #' @param provides list or environment. A list or environment of values
 #'   to provide to the file.
-#' @param execute logical or character. Whether or not to execute \code{filename}.
-#'   The default is \code{TRUE}, but if set to \code{FALSE}, no
-#'   element with key `value` will be returned in the `current` list.
-#'   Additionally, the character value "onchange" can be specified which
-#'   will execute the file if and only if its last modified time has change
-#'   since syberia's last attempt to access it.
-#' @return a two-element list with names `current` and `cached`, respectively.
-#'   Both will be lists containing keys `info` and `body`, but only the former
-#'   will contain `value`, the result of executing the R file specified
-#'   by the \code{filename} parameter. The `info` key holds the result of
-#'   of calling R's built-in \code{file.info} on the file (and includes
+#' @return a four-element list with names `current`, `cached`, `value`,
+#'   and `modified`. The former two will both be two-element lists containing
+#'   keys `info` and `body` (unless Syberia has never executed the file before
+#'   in which case the latter will be \code{NULL}). The `info` key holds the
+#'   result of calling R's built-in \code{file.info} on the file (and includes
 #'   information like created at and modified at time), whereas the `body`
 #'   key holds a character representation of the body of the file (useful
 #'   for comparing if any actual modifications have been made).
-syberia_resource <- function(filename, root = syberia_root(), provides = list(),
-                             execute = TRUE) {
+#'
+#'   The `value` key in the returned list holds a function which will
+#'   execute the given file in the \code{provides} environment. A function
+#'   is returned instead of the actual value to let the caller control
+#'   whether or not the file is executed (e.g., based on its properties
+#'   in the `current` list). Most frequently, this means consulting the
+#'   `modified` key of the returned list, which will hold a logical
+#'   indicating whether or not the file has been modified since last
+#'   executed by Syberia (if this was never the case, `modified` will
+#'   be \code{FALSE}).
+syberia_resource <- function(filename, root = syberia_root(), provides = list()) {
   resource_cache <- .get_registry_key('resource/resource_cache', root)
   resource_key <- function(filename, root) # Given a/b/c/d and a/b, extracts c/d
     substring(tmp <- normalizePath(filename), nchar(normalizePath(root)) + 1, nchar(tmp))
   resource_key <- resource_key(filename, root)
   cache_details <- resource_cache[[resource_key]]
 
-  if (!is.environment(provides)) provides <- as.environment(provides)
+  if (!is.environment(provides))
+    provides <- if (length(provides) == 0) new.env() else as.environment(provides)
   if (!file.exists(filename))
     stop("Syberia resource '", filename, "' in syberia project '", root,
          "' does not exist.", call. = FALSE)
 
   resource_info <- file.info(filename)
   resource_body <- paste(readLines(filename), collapse = "\n")
-  result <-
-    if (identical(execute, TRUE) ||
-         (identical(execute, "onchange") &&
-          resource_info$mtime > cache_details$info$mtime))
-      source(filename, local = provides)$value
 
   current_details <- list(
     info = resource_info,
     body = resource_body
   )
-  if (!is.null(result)) current_details$value <- result
 
-  resource_cache[[resource_ey]] <- 
-    current_details[-which(names(current_output) == 'value')]
-  .set_registry_key('resource/resources_cache', resource_cache, root)
+  resource_cache[[resource_key]] <- current_details
+  .set_registry_key('resource/resource_cache', resource_cache, root)
   # TODO: (RK) For large syberia projects, maybe this should dynamically
   # switch to tracking resources using the file system rather than one big list.
 
-  list(current = current_details, cached = cached_details)
+  value <- function() source(filename, local = provides)$value
+  modified <- resource_info$mtime > cache_details$info$mtime %||% 0
+
+  list(current = current_details, cached = cache_details,
+       value = value, modified = modified)
 }
 
 # TODO: (RK) It would be cool to dynamically look at the memory size of the object
